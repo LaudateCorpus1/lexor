7a4f247fabb4363c0ed31f27d88acfb7b40f766b
Manuel Lopez <jmlopez.rod@gmail.com>
2015-11-21 15:56:41 -0600

    develop command update
    removed print statement, used `disp`.

--- lexor/command/develop.py
+++ lexor/command/develop.py
@@ -4,12 +4,12 @@ Routine to append a path to the develop section in the configuration
 file.
 
 """
-
 import os
 import textwrap
 from imp import load_source
-from lexor.command import config, LexorError
+from lexor.command import disp, config, LexorError
 from lexor.util.logging import L
+
 
 DESC = """
 Append the path to the develop section in a configuration file.
@@ -28,7 +28,8 @@ def add_parser(subp, fclass):
 
 def run():
     """Append the path to the develop section in the configuration
-    file. """
+    file.
+    """
     cfg_file = config.read_config()
     arg = config.CONFIG['arg']
     path = os.path.abspath(arg.path)
@@ -61,5 +62,5 @@ def run():
     else:
         cfg_file.add_section('develop')
         cfg_file['develop'][key] = rel_path
-    print('%s --> %s' % (key, rel_path))
+    disp('%s --> %s\n', key, rel_path)
     config.write_config(cfg_file)
